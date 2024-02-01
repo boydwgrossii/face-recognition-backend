8e1af8ddea1207d1159258ec0a4d52cdd644cb20
boydwgrossii <118925231+boydwgrossii@users.noreply.github.com>
2024-02-01 15:30:17 -0600

    got to the test signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,44 +1,46 @@
 import React from 'react';
 
 class Signin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      signInEmail: '',
-      signInPassword: ''
+    constructor(props) {
+      super(props);
+      this.state = {
+        signInEmail: '',
+        signInPassword: ''
+      }
     }
-  }
 
-  onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
-  }
+    onEmailChange = (event) => {
+      this.setState({signInEmail: event.target.value})
+    }
 
-  onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
-  }
+    onPasswordChange = (event) => {
+      this.setState({signInPassword: event.target.value})
+    }
 
-  onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        email: this.state.signInEmail,
-        password: this.state.signInPassword
-      })
-    })
-      .then(response => response.json())
-      .then(user => {
-        if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
-        }
-      })
-  }
+    onSubmitSignIn = () => {
+      console.log(this.state);
+      this.props.onRouteChange('home');
+      // fetch('http://localhost:3000/signin', {
+      //   method: 'post',
+      //   headers: {'Content-Type': 'application/json'},
+      //   body: JSON.stringify({
+      //     email: this.state.signInEmail,
+      //     password: this.state.signInPassword
+      //   })
+      // })
+      //   .then(response => response.json())
+      //   .then(user => {
+      //     if (user.id) {
+      //       this.props.loadUser(user)
+      //       this.props.onRouteChange('home');
+      //     }
+      //   })
+    }
 
-  render() {
-    const { onRouteChange } = this.props;
-    return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+    render() {
+      const { onRouteChange } = this.props;
+      return (
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -81,5 +83,6 @@ class Signin extends React.Component {
     );
   }
 }
+
 
 export default Signin;
